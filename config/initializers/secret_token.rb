3c232460d80fd939b3c4dd415f012185d65853dd
Jon Newson <jon_newson@ieee.org>
2013-01-30 18:25:08 +1100

    Initial framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TerroirTimemachine::Application.config.secret_token = 'b980bb712fd39596e29568831687ef1c9259da0de8dd0c0e3917cd5331ebbe8030e86086664a62438062b357964e4d681c2543f475f805a2306bbb96e101b896'
